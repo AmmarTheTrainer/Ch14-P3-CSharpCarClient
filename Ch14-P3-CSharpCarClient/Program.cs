235b8bcc002b825daecf997a575c87c094c4e988
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-11-13 18:01:06 +0500

    final code update with git ignore

--- Ch14-P3-CSharpCarClient/Program.cs
+++ Ch14-P3-CSharpCarClient/Program.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Ch14_P2_CarLibrary;
+
 namespace Ch14_P3_CSharpCarClient
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("***** C# CarLibrary Client App *****");
+            // Make a sports car.
+            SportsCar viper = new SportsCar("Viper", 240, 40);
+            viper.TurboBoost();
+            // Make a minivan.
+            MiniVan mv = new MiniVan();
+            mv.TurboBoost();
+            Console.WriteLine("Done. Press any key to terminate");
+            Console.ReadLine();
         }
     }
 }
